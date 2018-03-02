e6a7497e9707f4762ddb4766e506a7ff6b6b3515
Srkinator <srki2207@gmail.com>
2018-03-02 14:07:38 +0100

    basic server with express done

---
+++ server/server.js
@@ -0,0 +1,8 @@
+console.log('server ')
+const express = require('express');
+const app = express();
+const server = app.listen(3000, listening);
+
+function listening (){
+    console.log("listening...");
+}
