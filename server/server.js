8a4c2bc32986ef7d8a47f7cf2ed62835c6aa3d4f
Srkinator <srki2207@gmail.com>
2018-03-05 14:48:15 +0100

    Delete method implemented

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const data = fs.readFileSync("./db/data.json");
 const groceries = JSON.parse(data);
+const http = require("http").Server(app);
 
-const server = app.listen(3000, listening);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (request, response, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.header(
+	response.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	response.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
 	);
@@ -36,6 +36,31 @@ function postRequestHandler(request, response) {
     response.send(newData);
 }
 
-function writeFileHandler(grocery) {
-    console.log(`Added Grocery : ${grocery.name}`)
+function writeFileHandler(grocery, reqType) {
+    if(reqType === 'delete'){
+        console.log(`Deleted Grocery : ${grocery.name}`);
+    } else{
+        console.log(`Added Grocery : ${grocery.name}`);
+    }
 }
+
+app.delete('/:name', deleteRequestHandler);
+function deleteRequestHandler(request, response){
+    let currentData = groceries;
+    let name = request.params.name;
+
+    for (let i = 0; i<currentData.length; i++){
+        let grocery = currentData[i];
+        if(name ===grocery.name){
+            currentData.splice(i, 1);
+        }
+    }
+
+    fs.writeFile('db/data.json', JSON.stringify(currentData, null, 2), writeFileHandler(newData, 'delete'));
+    response.send(`${name} is deleted from list`)
+}
+
+
+http.listen(3000, ()=>{
+    console.log('Server is running');
+});
