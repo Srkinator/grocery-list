bb789fb7ca72f7d038c114b522d7471cb36a5daa
Srkinator <srki2207@gmail.com>
2018-03-05 14:23:01 +0100

    Get and Post methods implemented

--- server/server.js
+++ server/server.js
@@ -1,8 +1,41 @@
-console.log('server ')
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const data = fs.readFileSync("./db/data.json");
+const groceries = JSON.parse(data);
+
 const server = app.listen(3000, listening);
 
-function listening (){
-    console.log("listening...");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (request, response, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
+app.get('/', getRequestHandler);
+function getRequestHandler(request,response) {
+    response.send(groceries);
+}
+
+app.post('/', postRequestHandler);
+function postRequestHandler(request, response) {
+    let currentData = groceries;
+    let newData = request.body;
+
+    Number(newData.quantity);
+
+    currentData.push(newData);
+    fs.writeFile('db/data.json', JSON.stringify(currentData, null, 2), writeFileHandler(newData));
+    response.send(newData);
+}
+
+function writeFileHandler(grocery) {
+    console.log(`Added Grocery : ${grocery.name}`)
 }
